a4b76950619cb663f6dc2054595fe841266308a1
Daniel Brown <lucha0075.db@gmail.com>
2023-09-01 06:22:54 +0100

    Update component to take style

--- src/components/utils/SectionHeading.tsx
+++ src/components/utils/SectionHeading.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, { CSSProperties } from 'react'
 
 interface SectionHeadingProps {
     // Add any props you might need
     heading: string;
+    style?: CSSProperties | undefined;
 }
 
-const SectionHeading = ({ heading }: SectionHeadingProps): JSX.Element => {
+const SectionHeading = ({ heading, style }: SectionHeadingProps): JSX.Element => {
 
     return (
-        <h2 className="section-heading">{heading}</h2>
+        <h2 style={style} className="section-heading">{heading}</h2>
     );
 };
 
